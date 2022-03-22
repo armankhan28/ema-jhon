04fbe8b0179767950f58817e61a664323fae7769
captainkhan28 <86075671+captainkhan28@users.noreply.github.com>
2022-03-22 23:41:14 +0600

    commit

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Shop.css'
+const Shop = () => {
+    return (
+        <div className='shop'>
+            <div className="product">
+                <h2>This is for products</h2>
+            </div>
+            <div className="cart">
+                <h2>Order Summary</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
